7a4970f93575713d8811c8ee6e78270bcd8d628d
Иван Васильев <65816571+VasilevIvanVladimirovich@users.noreply.github.com>
2021-09-27 13:02:19 +0300

    +

--- ProjectOpencv1/Part2.cpp
+++ ProjectOpencv1/Part2.cpp
@@ -10,29 +10,29 @@ using namespace cv;
 
 ////////////////// Basic func //////////////////
 
-int main()
-{
-    string path = "D:/PRoG/Git-repos/MyProjectOpenCV/ProjectOpencv1/Resourse/qVGtYwiqQWU.jpg";
-    Mat img = imread(path);
-    Mat imgGray, imgBlur, imgCanny, imgDil, imgErode;
+//int main()
+//{
+//    string path = "D:/PRoG/Git-repos/MyProjectOpenCV/ProjectOpencv1/Resourse/qVGtYwiqQWU.jpg";
+//    Mat img = imread(path);
+//    Mat imgGray, imgBlur, imgCanny, imgDil, imgErode;
 
-    cvtColor(img,imgGray,COLOR_BGR2GRAY);   // Серая фотография
-    GaussianBlur(img, imgBlur,Size(7,7),5,0);   //размытие по Гаусу
-    Canny(imgBlur,imgCanny,25,75);  //Контур
+//    cvtColor(img,imgGray,COLOR_BGR2GRAY);   // Серая фотография
+//    GaussianBlur(img, imgBlur,Size(7,7),5,0);   //размытие по Гаусу
+//    Canny(imgBlur,imgCanny,25,75);  //Контур
 
 
-    Mat karnel = getStructuringElement(MORPH_RECT, Size(3,3));
-    dilate(imgCanny,imgDil,karnel); //Увеличивает толщину контура
-    erode(imgDil, imgErode,karnel); //уменьшает толщину контура
+//    Mat karnel = getStructuringElement(MORPH_RECT, Size(3,3));
+//    dilate(imgCanny,imgDil,karnel); //Увеличивает толщину контура
+//    erode(imgDil, imgErode,karnel); //уменьшает толщину контура
 
-    imshow("img",img);
-    imshow("img Gray", imgGray);
-    imshow("img Blur", imgBlur);
-    imshow("img Canny", imgCanny);
-    imshow("img Dilation", imgDil);
-    imshow("img Erode", imgErode);
-    waitKey(0);
-}
+//    imshow("img",img);
+//    imshow("img Gray", imgGray);
+//    imshow("img Blur", imgBlur);
+//    imshow("img Canny", imgCanny);
+//    imshow("img Dilation", imgDil);
+//    imshow("img Erode", imgErode);
+//    waitKey(0);
+//}
 
 
 
---
+++ ProjectOpencv1/Part3.cpp
@@ -0,0 +1,33 @@
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+
+////////////////// Basic func //////////////////
+
+int main()
+{
+    string path = "D:/PRoG/Git-repos/MyProjectOpenCV/ProjectOpencv1/Resourse/qVGtYwiqQWU.jpg";
+    Mat img = imread(path);
+    Mat imgResize, imgCrop;
+
+    //cout<<img.size()<<endl;
+
+    resize(img,imgResize,Size(),0.5,0.5); //масштабирование
+
+    Rect roi(50,570,300,250);  //область интереса
+    imgCrop = img(roi);
+
+    imshow("img",img);
+    imshow("img Resize",imgResize);
+    imshow("img Crop",imgCrop);
+    waitKey(0);
+}
+
+
+
